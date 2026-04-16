1fb6f5644b3732fa6b0ba003e0b99cf626ada5cd
olloberdi <ohunpolatov.o@gmail.com>
2026-04-16 15:20:54 +0500

    5.kabisa yilini topadigan dastur yozdik.6.Foydalanuvchi ikkita son kiritadi biz ulardan kichigini topadigan dastur yozdik

--- main.js
+++ main.js
@@ -20,108 +20,110 @@
 //===============================================
 
 //4-masala   3-xonalik son kiritilsa uni sozlarda ifodalovchi dastur yozing
-let son = +prompt("3 xonalik son kiriting");
-let yuzlar = Math.trunc(son / 100);
-let onliklar = Math.trunc((son / 10) % 10);
-let birliklar = son % 10;
-let yuzlarSoz = "";
-let onliklarSoz = "";
-let birliklarSoz = "";
-switch (yuzlar) {
-  case 1:
-    yuzlarSoz = "bir yuz";
-    break;
-  case 2:
-    yuzlarSoz = "ikki yuz";
-    break;
-  case 3:
-    yuzlarSoz = "uch yuz";
-    break;
-  case 4:
-    yuzlarSoz = "tort yuz";
-    break;
-  case 5:
-    yuzlarSoz = "besh yuz";
-    break;
-  case 6:
-    yuzlarSoz = "olti yuz";
-    break;
-  case 7:
-    yuzlarSoz = "yetti yuz";
-    break;
-  case 8:
-    yuzlarSoz = "sakkiz yuz";
-    break;
-  case 9:
-    yuzlarSoz = "toqqiz yuz";
-    break;
-  default:
-    break;
-}
-switch (onliklar) {
-  case 1:
-    onliklarSoz = "o'n";
-    break;
-  case 2:
-    onliklarSoz = "yigirma";
-    break;
-  case 3:
-    onliklarSoz = "ottiz";
-    break;
-  case 4:
-    onliklarSoz = "qirq";
-    break;
-  case 5:
-    onliklarSoz = "ellik";
-    break;
-  case 6:
-    onliklarSoz = "oltmish";
-    break;
-  case 7:
-    onliklarSoz = "yetmish";
-    break;
-  case 8:
-    onliklarSoz = "sakson";
-    break;
-  case 9:
-    onliklarSoz = "toqson";
-    break;
-  default:
-    break;
-}
-switch (birliklar) {
-  case 1:
-    birliklarSoz = "bir";
-    break;
-  case 2:
-    birliklarSoz = "ikki";
-    break;
-  case 3:
-    birliklarSoz = "uch";
-    break;
-  case 4:
-    birliklarSoz = "tort";
-    break;
-  case 5:
-    birliklarSoz = "besh";
-    break;
-  case 6:
-    birliklarSoz = "olti";
-    break;
-  case 7:
-    birliklarSoz = "yetti";
-    break;
-  case 8:
-    birliklarSoz = "sakkiz";
-    break;
-  case 9:
-    birliklarSoz = "toqqiz";
-    break;
+//let son = +prompt("3 xonalik son kiriting");
+//let yuzlar = Math.trunc(son / 100);
+//let onliklar = Math.trunc((son / 10) % 10);
+//let birliklar = son % 10;
+//let yuzlarSoz = "";
+//let onliklarSoz = "";
+//let birliklarSoz = "";
+//switch (yuzlar) {
+//case 1:
+// yuzlarSoz = "bir yuz";
+// break;
+//case 2:
+//  yuzlarSoz = "ikki yuz";
+//  break;
+//case 3:
+// yuzlarSoz = "uch yuz";
+//  break;
+//case 4:
+//  yuzlarSoz = "tort yuz";
+// break;
+//case 5:
+//  yuzlarSoz = "besh yuz";
+//  break;
+// case 6:
+//   yuzlarSoz = "olti yuz";
+//   break;  case 7:
+//   yuzlarSoz = "yetti yuz";
+//   break;
+// case 8:
+//   yuzlarSoz = "sakkiz yuz";
+//   break;
+//case 9:
+//   case 0:
+//      yuzlarSoz=""
+// yuzlarSoz = "toqqiz yuz";
+// break;
+//default:
+//   break;}
+//switch (onliklar) {
+//  case 1:
+//   onliklarSoz = "o'n";
+//   break;
+// case 2:
+//  onliklarSoz = "yigirma";
+//   break;
+// case 3:
+//   onliklarSoz = "ottiz";
+//   break;
+// case 4:
+//   onliklarSoz = "qirq";
+//   break;
+// case 5:
+//   onliklarSoz = "ellik";
+//   break;
+//  case 6:
+//   onliklarSoz = "oltmish";
+//   break;
+// case 7:
+//   onliklarSoz = "yetmish";
+//   break;
+//case 8:
+//  onliklarSoz = "sakson";
+//   break;
+// case 9:
+//   onliklarSoz = "toqson";
+// case 0:
+//       onliklarSoz=""
+//   break;
+//  default:
+//   break;}
+//switch (birliklar) {
+//  case 1:
+//    birliklarSoz = "bir";
+//    break;
+//  case 2:
+//    birliklarSoz = "ikki";
+//    break;
+//  case 3:
+//    birliklarSoz = "uch";
+//   break;
+// case 4:
+//   birliklarSoz = "tort";
+//  break;
+// case 5:
+//  birliklarSoz = "besh";
+//   break;
+//  case 6:
+//  birliklarSoz = "olti";
+//   break;
+// case 7:
+//    birliklarSoz = "yetti";
+//    break;
+//  case 8:
+//  birliklarSoz = "sakkiz";
+//   break;
+// case 9:
+//   birliklarSoz = "toqqiz";
+//   break;
+//   case 0:
+//      birliklarSoz=""
 
-  default:
-    break;
-}
-console.log(`${yuzlarSoz}   ${onliklarSoz}  ${birliklarSoz}`);
+//default:
+//  break;}
+//console.log(`${yuzlarSoz}   ${onliklarSoz}  ${birliklarSoz}`);
 
 //===============================================
 
@@ -130,13 +132,19 @@ console.log(`${yuzlarSoz}   ${onliklarSoz}  ${birliklarSoz}`);
 // Mantiqiy yechim (Algoritm):Yilni $N$ deb olsak, u quyidagi mantiqiy zanjirdan o'tishi kerak:Agar $N$ soni 400 ga qoldiqsiz bo'linsa $\rightarrow$ bu Kabisa.
 // Agar 400 ga bo'linmasa, lekin 100 ga qoldiqsiz bo'linsa $\rightarrow$ bu Oddiy yil (chunki 100 ga bo'linib, 400 ga bo'linmaydigan yillar, masalan 1900 yoki 2100, kabisa emas)
 // .Agar yuqoridagilarning hech biri bo'lmasa, lekin 4 ga qoldiqsiz bo'linsa $\rightarrow$ bu Kabisa.Qolgan barcha holatlarda $\rightarrow$ Oddiy yil.
+//let yil = +prompt("yil kiriting");
+//if (yil % 400 === 0) { console.log("bu kabisa yili");}
+// else if (yil % 100 === 0) {console.log("bu oddiy yil");}
+// else if (yil % 4 === 0) { console.log("bu kabisa yili");}
+// else {console.log("bu oddiy yil");}
 //===============================================
-// 6-masala  foydalanuvchi 2ta son kiritadi ulardan eng kichigini qaytaruvchi dastur yozing bunda Math dan foydalanmang  budna || && operatorlaridan foydalaning
+// 6-masala  foydalanuvchi 2ta son kiritadi ulardan eng kichigini qaytaruvchi dastur yozing bunda Math dan foydalanmang  bunda || && operatorlaridan foydalaning
+let son1 = +prompt("1-sonni kiriting");
+let son2 = +prompt("2-sonni kiriting");
+son1 < son2 ? console.log(son1) : console.log(son2);
 
 //===============================================
 
 // 7-savol let const var kalit sozlarini bir birdan farqini yozib qoldiring
 
 //===============================================
-
-//
