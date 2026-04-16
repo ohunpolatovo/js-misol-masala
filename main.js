d4c7851d12d84778c868bff0bde8048127fc62b8
olloberdi <ohunpolatov.o@gmail.com>
2026-04-16 14:49:32 +0500

    foydalanuvchi uch honali son kiritadi biz uni sozlarda ifodalovchi dastur yozdik

--- main.js
+++ main.js
@@ -20,11 +20,116 @@
 //===============================================
 
 //4-masala   3-xonalik son kiritilsa uni sozlarda ifodalovchi dastur yozing
+let son = +prompt("3 xonalik son kiriting");
+let yuzlar = Math.trunc(son / 100);
+let onliklar = Math.trunc((son / 10) % 10);
+let birliklar = son % 10;
+let yuzlarSoz = "";
+let onliklarSoz = "";
+let birliklarSoz = "";
+switch (yuzlar) {
+  case 1:
+    yuzlarSoz = "bir yuz";
+    break;
+  case 2:
+    yuzlarSoz = "ikki yuz";
+    break;
+  case 3:
+    yuzlarSoz = "uch yuz";
+    break;
+  case 4:
+    yuzlarSoz = "tort yuz";
+    break;
+  case 5:
+    yuzlarSoz = "besh yuz";
+    break;
+  case 6:
+    yuzlarSoz = "olti yuz";
+    break;
+  case 7:
+    yuzlarSoz = "yetti yuz";
+    break;
+  case 8:
+    yuzlarSoz = "sakkiz yuz";
+    break;
+  case 9:
+    yuzlarSoz = "toqqiz yuz";
+    break;
+  default:
+    break;
+}
+switch (onliklar) {
+  case 1:
+    onliklarSoz = "o'n";
+    break;
+  case 2:
+    onliklarSoz = "yigirma";
+    break;
+  case 3:
+    onliklarSoz = "ottiz";
+    break;
+  case 4:
+    onliklarSoz = "qirq";
+    break;
+  case 5:
+    onliklarSoz = "ellik";
+    break;
+  case 6:
+    onliklarSoz = "oltmish";
+    break;
+  case 7:
+    onliklarSoz = "yetmish";
+    break;
+  case 8:
+    onliklarSoz = "sakson";
+    break;
+  case 9:
+    onliklarSoz = "toqson";
+    break;
+  default:
+    break;
+}
+switch (birliklar) {
+  case 1:
+    birliklarSoz = "bir";
+    break;
+  case 2:
+    birliklarSoz = "ikki";
+    break;
+  case 3:
+    birliklarSoz = "uch";
+    break;
+  case 4:
+    birliklarSoz = "tort";
+    break;
+  case 5:
+    birliklarSoz = "besh";
+    break;
+  case 6:
+    birliklarSoz = "olti";
+    break;
+  case 7:
+    birliklarSoz = "yetti";
+    break;
+  case 8:
+    birliklarSoz = "sakkiz";
+    break;
+  case 9:
+    birliklarSoz = "toqqiz";
+    break;
+
+  default:
+    break;
+}
+console.log(`${yuzlarSoz}   ${onliklarSoz}  ${birliklarSoz}`);
 
 //===============================================
 
 // 5-masala kabisa yilini aniqlovchi dastur yozing
-// Foydalanuvchi biror yilni (masalan: 2024, 2025, 2100) kiritadi. Dastur ushbu yil kabisa yoki oddiy yil ekanligini aniqlashi va natijani ekranga chiqarishi kerak.Mantiqiy yechim (Algoritm):Yilni $N$ deb olsak, u quyidagi mantiqiy zanjirdan o'tishi kerak:Agar $N$ soni 400 ga qoldiqsiz bo'linsa $\rightarrow$ bu Kabisa.Agar 400 ga bo'linmasa, lekin 100 ga qoldiqsiz bo'linsa $\rightarrow$ bu Oddiy yil (chunki 100 ga bo'linib, 400 ga bo'linmaydigan yillar, masalan 1900 yoki 2100, kabisa emas).Agar yuqoridagilarning hech biri bo'lmasa, lekin 4 ga qoldiqsiz bo'linsa $\rightarrow$ bu Kabisa.Qolgan barcha holatlarda $\rightarrow$ Oddiy yil.
+// Foydalanuvchi biror yilni (masalan: 2024, 2025, 2100) kiritadi. Dastur ushbu yil kabisa yoki oddiy yil ekanligini aniqlashi va natijani ekranga chiqarishi kerak.
+// Mantiqiy yechim (Algoritm):Yilni $N$ deb olsak, u quyidagi mantiqiy zanjirdan o'tishi kerak:Agar $N$ soni 400 ga qoldiqsiz bo'linsa $\rightarrow$ bu Kabisa.
+// Agar 400 ga bo'linmasa, lekin 100 ga qoldiqsiz bo'linsa $\rightarrow$ bu Oddiy yil (chunki 100 ga bo'linib, 400 ga bo'linmaydigan yillar, masalan 1900 yoki 2100, kabisa emas)
+// .Agar yuqoridagilarning hech biri bo'lmasa, lekin 4 ga qoldiqsiz bo'linsa $\rightarrow$ bu Kabisa.Qolgan barcha holatlarda $\rightarrow$ Oddiy yil.
 //===============================================
 // 6-masala  foydalanuvchi 2ta son kiritadi ulardan eng kichigini qaytaruvchi dastur yozing bunda Math dan foydalanmang  budna || && operatorlaridan foydalaning
 
