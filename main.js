b7c63b7cc63884bce6f3868bb124b81b94c79fbe
olloberdi <ohunpolatov.o@gmail.com>
2026-04-16 14:17:53 +0500

    sonning toq yoki juft ekanligini aniqlovchi dastur yozdik

---
+++ main.js
@@ -0,0 +1,25 @@
+// 1-masala sonning juft yoki toq son ekanligini aniqlovchi dastur yozing
+//let son = +prompt("son kiriting");
+//if (son % 2 === 0) {console.log("juft son");}
+// else {console.log("toq son");}
+//===============================================
+// 2-masala foydalanuvchidan 3-xonalik son oling ular ichida eng kattasi qaysi honada ekanligi aniqlovchi dastur yozing va consolega chiqaring
+//===============================================
+//3-maslaa sonning ishorasini aniqlovchi dastur yozing bunda console.log musbat manfiy yoki nol degan stringlar qaytarsin
+//===============================================
+
+//4-masala   3-xonalik son kiritilsa uni sozlarda ifodalovchi dastur yozing
+//===============================================
+
+// 5-masala kabisa yilini aniqlovchi dastur yozing
+// Foydalanuvchi biror yilni (masalan: 2024, 2025, 2100) kiritadi. Dastur ushbu yil kabisa yoki oddiy yil ekanligini aniqlashi va natijani ekranga chiqarishi kerak.Mantiqiy yechim (Algoritm):Yilni $N$ deb olsak, u quyidagi mantiqiy zanjirdan o'tishi kerak:Agar $N$ soni 400 ga qoldiqsiz bo'linsa $\rightarrow$ bu Kabisa.Agar 400 ga bo'linmasa, lekin 100 ga qoldiqsiz bo'linsa $\rightarrow$ bu Oddiy yil (chunki 100 ga bo'linib, 400 ga bo'linmaydigan yillar, masalan 1900 yoki 2100, kabisa emas).Agar yuqoridagilarning hech biri bo'lmasa, lekin 4 ga qoldiqsiz bo'linsa $\rightarrow$ bu Kabisa.Qolgan barcha holatlarda $\rightarrow$ Oddiy yil.
+//===============================================
+// 6-masala  foydalanuvchi 2ta son kiritadi ulardan eng kichigini qaytaruvchi dastur yozing bunda Math dan foydalanmang  budna || && operatorlaridan foydalaning
+
+//===============================================
+
+// 7-savol let const var kalit sozlarini bir birdan farqini yozib qoldiring
+
+//===============================================
+
+//
